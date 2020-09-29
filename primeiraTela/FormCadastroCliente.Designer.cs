27f14b46401f2cd226dde63b6bb080c78f44c27c
Daniel Vieira <62304465+danielvieiralopes@users.noreply.github.com>
2020-09-29 02:09:30 -0300

    Cadastro Cliente

--- primeiraTela/FormCadastroCliente.Designer.cs
+++ primeiraTela/FormCadastroCliente.Designer.cs
@@ -29,72 +29,176 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCadastroCliente));
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.panel1.SuspendLayout();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.textEdit3 = new DevExpress.XtraEditors.TextEdit();
+            this.textEdit4 = new DevExpress.XtraEditors.TextEdit();
+            this.textEdit5 = new DevExpress.XtraEditors.TextEdit();
+            this.textEdit6 = new DevExpress.XtraEditors.TextEdit();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit3.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit4.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit5.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit6.Properties)).BeginInit();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.pictureBox2);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 137);
-            this.panel1.TabIndex = 0;
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 137);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 194);
-            this.flowLayoutPanel1.TabIndex = 1;
-            // 
-            // panel2
-            // 
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 331);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(800, 119);
-            this.panel2.TabIndex = 2;
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(303, 0);
+            this.pictureBox2.Location = new System.Drawing.Point(316, 18);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(141, 120);
+            this.pictureBox2.Size = new System.Drawing.Size(132, 111);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.EditValue = "Nome";
+            this.textEdit1.Location = new System.Drawing.Point(100, 199);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Properties.Appearance.ForeColor = System.Drawing.Color.Gray;
+            this.textEdit1.Properties.Appearance.Options.UseForeColor = true;
+            this.textEdit1.Size = new System.Drawing.Size(284, 20);
+            this.textEdit1.TabIndex = 3;
+            // 
+            // textEdit2
+            // 
+            this.textEdit2.EditValue = "CPF";
+            this.textEdit2.Location = new System.Drawing.Point(412, 199);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Properties.Appearance.ForeColor = System.Drawing.Color.Gray;
+            this.textEdit2.Properties.Appearance.Options.UseForeColor = true;
+            this.textEdit2.Size = new System.Drawing.Size(248, 20);
+            this.textEdit2.TabIndex = 4;
+            // 
+            // textEdit3
+            // 
+            this.textEdit3.EditValue = "Telefone";
+            this.textEdit3.Location = new System.Drawing.Point(100, 238);
+            this.textEdit3.Name = "textEdit3";
+            this.textEdit3.Properties.Appearance.ForeColor = System.Drawing.Color.Gray;
+            this.textEdit3.Properties.Appearance.Options.UseForeColor = true;
+            this.textEdit3.Size = new System.Drawing.Size(153, 20);
+            this.textEdit3.TabIndex = 5;
+            // 
+            // textEdit4
+            // 
+            this.textEdit4.EditValue = "Nascimento";
+            this.textEdit4.Location = new System.Drawing.Point(292, 238);
+            this.textEdit4.Name = "textEdit4";
+            this.textEdit4.Properties.Appearance.ForeColor = System.Drawing.Color.Gray;
+            this.textEdit4.Properties.Appearance.Options.UseForeColor = true;
+            this.textEdit4.Size = new System.Drawing.Size(156, 20);
+            this.textEdit4.TabIndex = 6;
+            // 
+            // textEdit5
+            // 
+            this.textEdit5.EditValue = "Situação";
+            this.textEdit5.Location = new System.Drawing.Point(490, 238);
+            this.textEdit5.Name = "textEdit5";
+            this.textEdit5.Properties.Appearance.ForeColor = System.Drawing.Color.Gray;
+            this.textEdit5.Properties.Appearance.Options.UseForeColor = true;
+            this.textEdit5.Size = new System.Drawing.Size(170, 20);
+            this.textEdit5.TabIndex = 7;
+            // 
+            // textEdit6
+            // 
+            this.textEdit6.EditValue = "Endereço";
+            this.textEdit6.Location = new System.Drawing.Point(100, 282);
+            this.textEdit6.Name = "textEdit6";
+            this.textEdit6.Properties.Appearance.ForeColor = System.Drawing.Color.Gray;
+            this.textEdit6.Properties.Appearance.Options.UseForeColor = true;
+            this.textEdit6.Size = new System.Drawing.Size(560, 20);
+            this.textEdit6.TabIndex = 8;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.LoginButton.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginButton.Image = ((System.Drawing.Image)(resources.GetObject("LoginButton.Image")));
+            this.LoginButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.LoginButton.Location = new System.Drawing.Point(204, 367);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.LoginButton.Size = new System.Drawing.Size(168, 36);
+            this.LoginButton.TabIndex = 9;
+            this.LoginButton.Text = "Cadastrar";
+            this.LoginButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.LoginButton.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(412, 367);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(168, 36);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Cancelar";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(326, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Adicione uma imagem";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // FormCadastroCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.flowLayoutPanel1);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.textEdit6);
+            this.Controls.Add(this.textEdit5);
+            this.Controls.Add(this.textEdit4);
+            this.Controls.Add(this.textEdit3);
+            this.Controls.Add(this.textEdit2);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.pictureBox2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormCadastroCliente";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit3.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit4.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit5.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit6.Properties)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.TextEdit textEdit3;
+        private DevExpress.XtraEditors.TextEdit textEdit4;
+        private DevExpress.XtraEditors.TextEdit textEdit5;
+        private DevExpress.XtraEditors.TextEdit textEdit6;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- primeiraTela/FormCadastroCliente.cs
+++ primeiraTela/FormCadastroCliente.cs
@@ -16,5 +16,15 @@ namespace primeiraTela
         {
             InitializeComponent();
         }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
