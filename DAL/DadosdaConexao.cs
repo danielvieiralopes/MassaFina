f6bc006c4c3c2cba79b8c5e1a69f6a970a77377e
Daniel Vieira <62304465+danielvieiralopes@users.noreply.github.com>
2020-09-30 23:26:35 -0300

    Criacao e conexao com banco de dados, crud realizado e estruturado camadas do projeto

---
+++ DAL/DadosdaConexao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+   public class DadosdaConexao
+    {
+       public static String StringDeConexao
+        {
+            get
+            {
+                return "Data Source=M4rkSeven;Initial Catalog=MassaFina;Persist Security Info=True;User ID=sa;Password=marob0107";
+            }
+        }
+    }
+}
